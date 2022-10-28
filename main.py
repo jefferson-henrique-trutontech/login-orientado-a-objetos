1e861af6543bd64f7a793f5823f4d7e994ffe4c0
jefferson-henrique-trutontech <jefersson891@gmail.com>
2022-10-27 23:09:11 -0300

    Login adicionado ao sistema

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class Database():
     def __init__(self, banco):
         self.con = sq.connect(banco)
         self.cur = self.con.cursor()
+        self.users = self.cur.execute("SELECT * FROM usuarios").fetchall()
 
 
 class App(sg.Window):
@@ -22,14 +23,29 @@ class App(sg.Window):
 
             if self.ev == sg.WIN_CLOSED:
                 break
+            elif self.ev == 'Entrar':
+                for user in self.database.users:
+                    name = user[1]
+                    passw = user[2]
+                    login = name.lower() == self.vl['inpNome'].lower() and passw == self.vl['inpSenha']
+                    if login:
+                        self['output'].update('Logado com sucesso')
+                        break
+                    else:
+                        self['output'].update('Erro no login')
+
+                self['inpNome'].update('')
+                self['inpSenha'].update('')
+                pass
 
     def templateLayout(self):
 
         layout = [
             [sg.Text('APLICATIVO DE LOGIN')],
             [sg.Text('User: '), sg.Input(key='inpNome', expand_x=True)],
-            [sg.Text('Pass: '), sg.Input(key='inpSenha', password_char='*', expand_x=True)],
-            [sg.Text('', expand_x=True), sg.Button('Entrar')]
+            [sg.Text('Pass: '), sg.Input(key='inpSenha',
+                                         password_char='*', expand_x=True)],
+            [sg.Text('',key='output', expand_x=True), sg.Button('Entrar')]
         ]
 
         return layout
